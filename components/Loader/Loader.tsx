3aaa85557f6edf1abf0a99505bc3e9b757bb5251
tomasborek <tomasborek3@gmail.com>
2022-08-09 12:09:13 +0200

    Do loaderu byl přidán gif a bylo zakázáno scrollování

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -3,7 +3,11 @@ import style from "./loader.module.scss";
 const Loader = () => {
   return (
     <div className={style.loader}>
-      <img src={"/img/logos/kontejner-logo.png"} />
+      <img src={"/media/neon.webp"} />
+      <picture>
+        <source src="/media/neon.webp" type="picture/webp" />
+        <source src="/logos/kontejner-logo.png" type="picture/png" />
+      </picture>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,17 @@ import Head from "next/head";
 import "../styles/global.scss";
 import MenuProvider from "../contexts/MenuContext";
 import Loader from "../components/Loader/Loader";
+
 function MyApp({ Component, pageProps }) {
   const [loader, setLoader] = useState<boolean>(true);
   useEffect(() => {
+    document.querySelector("body").style.overflowY = "hidden";
     setTimeout(() => {
       setLoader(false);
+      document.querySelector("body").style.overflowY = "scroll";
     }, 1500);
   }, []);
+
   return (
     <>
       <Head>
@@ -21,6 +25,11 @@ function MyApp({ Component, pageProps }) {
           integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/img/logos/kontejner-logo-favicon.png"
         />
       </Head>
 
