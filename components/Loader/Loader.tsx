87f16fa6a02f5cf90f99d0eb7b7d7520eb6671c0
tomasborek <tomasborek3@gmail.com>
2022-08-11 15:07:38 +0200

    Loader fixed

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -3,7 +3,6 @@ import style from "./loader.module.scss";
 const Loader = () => {
   return (
     <div className={style.loader}>
-      <img src={"/media/neon.webp"} />
       <picture>
         <source src="/media/neon.webp" type="picture/webp" />
         <source src="/logos/neon.gif" type="picture/gif" />
