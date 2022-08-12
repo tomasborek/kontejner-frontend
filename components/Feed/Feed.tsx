97d40e12adeab4b75bbf5ac91b0a648bd7a2e4b6
tomasborek <tomasborek3@gmail.com>
2022-08-12 12:20:49 +0200

    IG feed byl prozatimně vykomentován kvůli build errorum

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 
 const Feed = () => {
-  useEffect(() => {
-    window.PixleeAsyncInit = function () {
-      Pixlee.init({ apiKey: "kJbI_trUw70SimutKbVr" });
-      Pixlee.addSimpleWidget({ widgetId: "36854" });
-    };
-    const scriptTag = document.createElement("script");
-    scriptTag.src =
-      "//instafeed.assets.pxlecdn.com/assets/pixlee_widget_1_0_0.js";
-    document.body.appendChild(scriptTag);
-  }, []);
+  // useEffect(() => {
+  //   window.PixleeAsyncInit = function () {
+  //     Pixlee.init({ apiKey: "kJbI_trUw70SimutKbVr" });
+  //     Pixlee.addSimpleWidget({ widgetId: "36854" });
+  //   };
+  //   const scriptTag = document.createElement("script");
+  //   scriptTag.src =
+  //     "//instafeed.assets.pxlecdn.com/assets/pixlee_widget_1_0_0.js";
+  //   document.body.appendChild(scriptTag);
+  // }, []);
   return (
     <div className="container">
       <div style={{ width: "100%" }} id="pixlee_container" />
